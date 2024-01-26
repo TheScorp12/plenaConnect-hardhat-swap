3f452ae13bb4c896492879c2cde54b2bbe7a43c8
TheScorp12 <aasimandaasim@gmail.com>
2024-01-26 19:00:04 +0530

    initial commit

--- test/swap.test.js
+++ test/swap.test.js
@@ -12,12 +12,11 @@ const {
 } = require("@uniswap/sdk-core");
 const { JSBI, BigInt } = require("jsbi");
 
-const USDT = "0xc2132D05D31c914a87C6611C10748AEb04B58e8F";
 const USDC_HOLDER = "0x443173a5440eeCB6960D6d73FD3C3D8a87E368f3";
-const AAVE_V3_POOL_POLYGON = "0x794a61358D6845594F94dc1DB02A252b5b4814aD";
 const chainId = 137; //for polygon
-Provider = new hre.ethers.providers.JsonRpcProvider('http://127.0.0.1:8545/')
+Provider = new hre.ethers.providers.JsonRpcProvider('http://127.0.0.1:8545/') // chnage for tenderly 
 const router = new AlphaRouter({ chainId: chainId, provider: Provider });
+
 //Token info on polygon
 const nameUSDC = "USD Coin";
 const symbolUSDC = "USDC";
@@ -53,37 +52,6 @@ contract("PlenaConnectTest", ([]) => {
       params: [USDC_HOLDER],
     });
 
-    //aave v3 pool abi
-    const aaveABI = [
-      {
-        inputs: [
-          {
-            internalType: "address",
-            name: "asset",
-            type: "address",
-          },
-          {
-            internalType: "uint256",
-            name: "amount",
-            type: "uint256",
-          },
-          {
-            internalType: "address",
-            name: "onBehalfOf",
-            type: "address",
-          },
-          {
-            internalType: "uint16",
-            name: "referralCode",
-            type: "uint16",
-          },
-        ],
-        name: "supply",
-        outputs: [],
-        stateMutability: "nonpayable",
-        type: "function",
-      },
-    ];
 
     //erc 20 abi
     const erc20ABI = [
@@ -151,6 +119,7 @@ contract("PlenaConnectTest", ([]) => {
   });
 
   it("should swap USDC TO USDT", async function () {
+    //finding the optimal swap route
     const wei = ethers.utils.parseUnits("10", 6);
     const inputAmount = CurrencyAmount.fromRawAmount(tokenUSDC, BigInt(wei));
     var route = await router.route(inputAmount, tokenUSDT, TradeType.EXACT_INPUT, {
@@ -160,6 +129,7 @@ contract("PlenaConnectTest", ([]) => {
         deadline: Math.floor(Date.now() / 1000 + 1800),
       });
     console.log(`amount is: ${route.quote.toFixed(6)}`);
+
     //Encode Function Data for Approve
     const approvalAmount = hre.ethers.utils.parseUnits("11", 6).toString();
     const approveData = erc20Contract.encodeFunctionData("approve", [
@@ -168,10 +138,12 @@ contract("PlenaConnectTest", ([]) => {
     ]);
     // console.log("approveData", approveData);
 
+    //log balances before swap
     var balance = await usdc.balanceOf(wallet.address);
     console.log(`balance of USDC is ${balance}`);
     balance = await usdt.balanceOf(wallet.address);
     console.log(`balance of USDT is ${balance}`)
+
     //Draft Transaction
     const tx = await wallet.executeCall(
       [addressUSDC, '0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45'], //Smart Contract Addresses On which the transactions has to be made
@@ -183,6 +155,8 @@ contract("PlenaConnectTest", ([]) => {
         value: '0',
       }
     );
+    
+    //log balances after swap
     balance = await usdc.balanceOf(wallet.address);
     console.log(`new balance of USDC is ${balance}`)
     balance = await usdt.balanceOf(wallet.address);
